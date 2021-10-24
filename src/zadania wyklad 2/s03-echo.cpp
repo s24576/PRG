f5e2622ee5fff3b66744bcf378683067d146e93a
s24576 <91957033+s24576@users.noreply.github.com>
2021-10-24 18:20:09 +0200

    Update s03-echo.cpp

--- src/zadania wyklad 2/s03-echo.cpp
+++ src/zadania wyklad 2/s03-echo.cpp
@@ -3,44 +3,100 @@
 
 int main(int argc, char* argv[])
 {
-if (argc == 0)
+if ( argc == 1 )
 {
-return 1;
+    std::cout<<"Blad danych"<<"\n";
+    return 1;
 }
 auto const polecenie = std::string{argv[1]};
-if(argc == 
-if(polecenie[0]=='-')
+if( argc > 2 )
 {
-if(polecenie[1]=='n')
-{
+    //warunek polecenia
+    if( polecenie[0] == '-' )
+    {
+//polecenie N
+        if( polecenie[1] == 'n' )
+        {
+            for( int i=2; i<argc; i++ )
+            {
+                auto const wyraz = std::string{argv[i]};
+                std::cout<< wyraz << " " ;
+            }
+        }
 
-}
-if(polecenie[1]=='r')
-{
-
-auto const polecenie2 = std::string{argv[2]};
-	if(polecenie2[0]=='-')
-		{
-		if(polecenie2[1]=='l';
-
-		if(polecenie2[1]=='n';
-
-		}
-}
-if(polecenie[1]=='l')
-{
-
-}
+//polecenie R
+        if( polecenie[1] == 'r' )
+        {
+            if(argc > 3)
+            {
+                auto const polecenie2 = std::string{argv[2]};
+                //drugie polecenie
+                if( polecenie2[0] == '-' )
+                {
+                    if( polecenie2[1] == 'l' )
+                    {
+                        for(int i=argc; i>2; i--)
+                        {
+                            auto const wyraz = std::string{argv[i]};
+                            std::cout << wyraz << "/n";
+                        }
+                    }
+                    if( polecenie2[1] == 'n' )
+                    {
+                        for(int i=argc; i>2; i--)
+                        {
+                             auto const wyraz = std::string{argv[i]};
+                             std::cout<< wyraz << "  ";
+                        }
+                    }
+                }
+                else
+                {
+                    for(int i=argc; i>0; i--)
+                    {
+                        auto const wyraz = std::string{argv[i]};
+                        std::cout<< wyraz << "  ";
+                    }
+                }
+            }
+            else
+            {
+                for(int i=argc; i>1; i--)
+                {
+                    auto const wyraz = std::string{argv[i]};
+                    std::cout<< wyraz << "  ";
+                }
+            }
+        }
+//polecenie L
+        if(polecenie[1]=='l')
+        {
+            for (int i=2;i<argc;i++)
+            {
+                auto const wyraz = std::string{argv[i]};
+                std::cout << wyraz <<"\n";
+            }
+        }
+    }
+    else
+    {
+        for(int i=2; i>argc; i++)
+        {
+            auto const wyraz = std::string{argv[i]};
+            std::cout<< wyraz << "  ";
+        }
+    }
 }
 else
 {
-
+    auto const wyraz = std::string{argv[1]};
+    std::cout << wyraz <<"\n";
 }
-for (int i=1;i<argc;i++)
+/*for (int i=1;i<argc;i++)
 {
 auto const name = std::string{argv[i]};
 std::cout << name <<"\n";
 
-}
+}*/
 return 0;
 }
