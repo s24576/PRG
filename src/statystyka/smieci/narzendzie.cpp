10ea315a7c39d412b25eeb563f6b9e4cdc499632
s24576 <91957033+s24576@users.noreply.github.com>
2021-12-28 19:56:45 +0100

    Add files via upload

---
+++ src/statystyka/smieci/narzendzie.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    fstream wczytaj;
+    fstream zapisz;
+    string d1;
+    vector <string> oddaje;
+    int d;
+    wczytaj.open("ptottenham.txt",ios::in);
+    zapisz.open("stot.txt",ios::out);
+    while(!wczytaj.eof())
+    {
+        wczytaj>>d1;
+        if((int)d1[0]>='0'&&(int)d1[0]<='9')
+                oddaje.push_back(d1);
+        if(d1=="minuty")
+            oddaje.pop_back();
+        if(d1=="czerwona")
+            oddaje.pop_back();
+    }
+    d=oddaje.size();
+    for(int i=0;i<d;i++)
+    {
+            d1=oddaje[i];
+            zapisz<<d1<<endl;
+    }
+    return 0;
+}
